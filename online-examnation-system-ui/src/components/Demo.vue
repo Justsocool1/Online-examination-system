987f8e10953b6e4cb24f3937ff263821f6bdbaee
justsocool <linyudaf@qq,com>
2022-03-05 10:51:23 +0800

    创建前端VUE工程  review by songshitao

---
+++ online-examnation-system-ui/src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a-input-number id="inputNumber" v-model="value" :min="1" :max="10" @change="onChange" />
+    当前值：{{ value }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: 3,
+    };
+  },
+  methods: {
+    onChange(value) {
+      console.log('changed', value);
+    },
+  },
+};
+</script>
---
+++ online-examnation-system-ui/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <home-page></home-page>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Demo from '@/components/Demo.vue'
+import HomePage from '@/components/HomePage.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Demo,
+    HomePage
+  }
+}
+</script>
