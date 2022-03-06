e3e5334c6bc88b44bc50c766cd36b7c50de4a6b9
justsocool <linyudaf@qq,com>
2022-03-06 13:39:46 +0800

    创建后端工程 review by lins

---
+++ online-examination-system-api/src/main/java/com/lins/OnlineExaminationSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.lins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExaminationSystemApiApplication.class, args);
+    }
+
+}
---
+++ online-examination-system-api/src/test/java/com/lins/OnlineExaminationSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
